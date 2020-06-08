b717877bcbed41aa69bcc3c04ffcaf6cda4e0029
ryoken nakayama <ryoken7090@gmail.com>
2020-06-08 15:25:57 +0900

    rootの設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   resources :favorites, only: [:create, :destroy]
+  root to: 'stories#index'
+
 end
